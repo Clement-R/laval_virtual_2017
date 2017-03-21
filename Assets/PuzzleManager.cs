c5c49d678a7342a6c7b5326e088cd583cf1bdc5d
C.Rondeau <rondeau.clement@gmail.com>
2017-03-21 21:44:22 +0100

    Start to work on the game manager to handle puzzle completion

---
+++ Assets/PuzzleManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class PuzzleManager : MonoBehaviour {
+    public List<VRTK_Button> buttons = new List<VRTK_Button>();
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		if(buttons.TrueForAll(e => e.done == true)) {
+            Debug.Log("Job's done");
+        }
+	}
+
+    public void UpdateButton(VRTK_Button button) {
+        int but = buttons.FindIndex(e => e.GetInstanceID() == button.GetInstanceID());
+        buttons[but].active = false;
+    }
+}
