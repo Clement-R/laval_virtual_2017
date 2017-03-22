5eadc39eddb84ed3bd9c3dbfefa5077f182482b5
C.Rondeau <rondeau.clement@gmail.com>
2017-03-22 01:25:47 +0100

    Add new conditions in puzzle manager for doneOnNext

--- Assets/PuzzleManager.cs
+++ Assets/PuzzleManager.cs
@@ -15,14 +15,41 @@ public class PuzzleManager : MonoBehaviour {
     public void UpdateButton(VRTK_Button button) {
         int id = buttons.FindIndex(e => e.GetInstanceID() == button.GetInstanceID());
 
-        buttons[id].active = false;
-        buttons[id].GetComponent<VRTK_InteractableObject>().enabled = false;
-        buttons[id].GetComponent<MeshRenderer>().material.color = Color.green;
+        // Don't try to access element before 0
+        if (id - 1 > 0) {
+            // If the previous one needed the next one to be completed
+            if (buttons[id - 1].doneOnNext && buttons[id - 1].ReachedActivationDistance()) {
+                Debug.Log(buttons[id - 1].ReachedActivationDistance());
 
-        if (id != buttons.Count - 1) {
-            buttons[id + 1].active = true;
-            buttons[id + 1].GetComponent<VRTK_InteractableObject>().enabled = true;
-            buttons[id + 1].GetComponent<MeshRenderer>().material.color = Color.yellow;
+                buttons[id - 1].active = false;
+                buttons[id - 1].done = true;
+                buttons[id - 1].GetComponent<VRTK_InteractableObject>().enabled = false;
+                buttons[id - 1].GetComponent<MeshRenderer>().material.color = Color.green;
+
+                buttons[id].active = false;
+                buttons[id].GetComponent<VRTK_InteractableObject>().enabled = false;
+                buttons[id].GetComponent<MeshRenderer>().material.color = Color.green;
+
+                if (id != buttons.Count - 1) {
+                    buttons[id + 1].active = true;
+                    buttons[id + 1].GetComponent<VRTK_InteractableObject>().enabled = true;
+                    buttons[id + 1].GetComponent<MeshRenderer>().material.color = Color.yellow;
+                }
+            } else {
+                buttons[id].done = false;
+            }
+        } else {
+            if (!buttons[id].doneOnNext) {
+                buttons[id].active = false;
+                buttons[id].GetComponent<VRTK_InteractableObject>().enabled = false;
+                buttons[id].GetComponent<MeshRenderer>().material.color = Color.green;
+            }
+
+            if (id != buttons.Count - 1) {
+                buttons[id + 1].active = true;
+                buttons[id + 1].GetComponent<VRTK_InteractableObject>().enabled = true;
+                buttons[id + 1].GetComponent<MeshRenderer>().material.color = Color.yellow;
+            }
         }
     }
 }
